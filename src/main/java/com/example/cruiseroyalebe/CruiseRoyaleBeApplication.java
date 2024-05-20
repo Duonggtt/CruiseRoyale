2d05bdd3b4b5d4f98ee598ebca687fb55952796b
Duonggtt <114550227+Duonggtt@users.noreply.github.com>
2024-05-20 16:04:47 +0700

    Initial commit

---
+++ src/main/java/com/example/cruiseroyalebe/CruiseRoyaleBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cruiseroyalebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruiseRoyaleBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruiseRoyaleBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cruiseroyalebe/CruiseRoyaleBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cruiseroyalebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruiseRoyaleBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
