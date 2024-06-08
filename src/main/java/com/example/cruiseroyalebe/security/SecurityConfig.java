153aa0ca6096df48d625b612045b58572972ce85
Duonggtt <114550227+Duonggtt@users.noreply.github.com>
2024-06-08 20:26:16 +0700

    api-authen-location

--- src/main/java/com/example/cruiseroyalebe/security/SecurityConfig.java
+++ src/main/java/com/example/cruiseroyalebe/security/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         http.authorizeRequests().antMatchers(
                 "/api/login/**", "/api/token/refresh/**", "/cruise-royal/login", "/api/user/save/**",
-                "/api/user/**", "/api/cruises/featured", "/api/cruises/", "/api/cruise/images/", "/api/cruise/images/**"
+                "/api/user/**", "/api/cruises/featured", "/api/cruises/", "/api/cruise/images/", "/api/cruise/images/**",
+                "/api/locations/**"
         ).permitAll();
         http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
